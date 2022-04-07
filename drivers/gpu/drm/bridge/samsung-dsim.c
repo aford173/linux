6d263ebf291786634dc26b229284ca1f2c5373fc
Jagan Teki <jagan@amarulasolutions.com>
2022-04-07 18:29:51 +0530

    drm: bridge: samsung-dsim: Add input_bus_flags
    eLCDIF is expecting to have input_bus_flags as DE_LOW in order to
    set active low during valid data transfer on each horizontal line.
    
    Add DE_LOW flag via drm bridge timings.
    
    v3, v2:
    * none
    
    v1:
    * none
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/bridge/samsung-dsim.c
+++ drivers/gpu/drm/bridge/samsung-dsim.c
@@ -1591,6 +1591,10 @@ __weak void samsung_dsim_plat_remove(struct samsung_dsim *priv)
 {
 }
 
+static const struct drm_bridge_timings samsung_dsim_bridge_timings = {
+	.input_bus_flags = DRM_BUS_FLAG_DE_LOW,
+};
+
 static int samsung_dsim_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -1670,6 +1674,7 @@ static int samsung_dsim_probe(struct platform_device *pdev)
 
 	dsi->bridge.funcs = &samsung_dsim_bridge_funcs;
 	dsi->bridge.of_node = dev->of_node;
+	dsi->bridge.timings = &samsung_dsim_bridge_timings;
 	dsi->bridge.type = DRM_MODE_CONNECTOR_DSI;
 
 	if (dsi->driver_data->quirks & DSIM_QUIRK_PLAT_DATA)
