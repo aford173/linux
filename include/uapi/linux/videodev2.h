ddf85c5d3ff08614a3a73495b357ac2bdb9093c4
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2023-03-03 11:36:34 +0100

    media: uapi: Add VP8 stateless encoder controls
    Add uAPI for stateless VP8 encoders.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -1836,6 +1836,7 @@ struct v4l2_ext_control {
 		struct v4l2_ctrl_av1_tile_group_entry __user *p_av1_tile_group_entry;
 		struct v4l2_ctrl_av1_frame __user *p_av1_frame;
 		struct v4l2_ctrl_av1_film_grain __user *p_av1_film_grain;
+		struct v4l2_ctrl_vp8_encode_params __user *p_vp8_encode_params;
 		void __user *ptr;
 	};
 } __attribute__ ((packed));
@@ -1914,6 +1915,8 @@ enum v4l2_ctrl_type {
 	V4L2_CTRL_TYPE_AV1_TILE_GROUP_ENTRY = 0x281,
 	V4L2_CTRL_TYPE_AV1_FRAME	    = 0x282,
 	V4L2_CTRL_TYPE_AV1_FILM_GRAIN	    = 0x283,
+
+	V4L2_CTRL_TYPE_VP8_ENCODE_PARAMS	= 0x0290,
 };
 
 /*  Used in the VIDIOC_QUERYCTRL ioctl for querying controls */
